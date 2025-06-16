4b07b312e80f71fc18e7cf218cbbbfb7f5183d06
Jenna <148486734+jennarms@users.noreply.github.com>
2025-06-17 02:07:26 +0800

    edited database, inserted all possible accounts

--- MetroLayag/Data/ApplicationDbContext.cs
+++ MetroLayag/Data/ApplicationDbContext.cs
@@ -12,21 +12,28 @@ namespace MetroLayag.Data
 
         public DbSet<Passenger> Passengers { get; set; }
         public DbSet<User> Users { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // Seed Main Admin user
-            var passwordHash = "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu";
+            var passwordHash = "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu"; // hashed "pass123"
+
             modelBuilder.Entity<User>().HasData(
-                new User
-                {
-                    Id = 1,
-                    Station = "Main Office",
-                    Username = "admin",
-                    PasswordHash = passwordHash,
-                    Role = "MainAdmin"
-                }
+                new User { Id = 1, Station = "Main Office", Username = "admin", PasswordHash = passwordHash, Role = "MainAdmin" },
+                new User { Id = 2, Station = "Escolta", Username = "escolta", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 3, Station = "Lawton", Username = "lawton", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 4, Station = "Quinta", Username = "quinta", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 5, Station = "PUP", Username = "pup", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 6, Station = "Sta. Ana", Username = "staana", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 7, Station = "Lambingan", Username = "lambingan", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 8, Station = "Valenzuela", Username = "valenzuela", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 9, Station = "Hulo", Username = "hulo", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 10, Station = "Guadalupe", Username = "guadalupe", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 11, Station = "Maybunga", Username = "maybunga", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 12, Station = "San Joaquin", Username = "sanjoaquin", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 13, Station = "Kalawaan", Username = "kalawaan", PasswordHash = passwordHash, Role = "StationAdmin" },
+                new User { Id = 14, Station = "Pinagbuhatan", Username = "pinagbuhatan", PasswordHash = passwordHash, Role = "StationAdmin" }
             );
         }
     }
---
+++ MetroLayag/Migrations/20250616180636_InitialCreate.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MetroLayag.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Passengers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    StartingStation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Destination = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCanceled = table.Column<bool>(type: "bit", nullable: false),
+                    HasDisembarked = table.Column<bool>(type: "bit", nullable: false),
+                    BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passengers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Station = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "PasswordHash", "Role", "Station", "Username" },
+                values: new object[,]
+                {
+                    { 1, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "MainAdmin", "Main Office", "admin" },
+                    { 2, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Escolta", "escolta" },
+                    { 3, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Lawton", "lawton" },
+                    { 4, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Quinta", "quinta" },
+                    { 5, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "PUP", "pup" },
+                    { 6, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Sta. Ana", "staana" },
+                    { 7, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Lambingan", "lambingan" },
+                    { 8, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Valenzuela", "valenzuela" },
+                    { 9, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Hulo", "hulo" },
+                    { 10, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Guadalupe", "guadalupe" },
+                    { 11, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Maybunga", "maybunga" },
+                    { 12, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "San Joaquin", "sanjoaquin" },
+                    { 13, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Kalawaan", "kalawaan" },
+                    { 14, "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu", "StationAdmin", "Pinagbuhatan", "pinagbuhatan" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Passengers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
