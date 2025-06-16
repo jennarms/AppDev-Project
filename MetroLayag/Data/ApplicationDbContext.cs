5f4be07e3abf3b909daf4b605038f29a8d99bd10
Jenna <148486734+jennarms@users.noreply.github.com>
2025-06-16 22:25:54 +0800

    Created new table and manually inserted user 1

--- MetroLayag/Data/ApplicationDbContext.cs
+++ MetroLayag/Data/ApplicationDbContext.cs
@@ -11,5 +11,23 @@ namespace MetroLayag.Data
         }
 
         public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed Main Admin user
+            var passwordHash = "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu";
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Station = "Main Office",
+                    Username = "admin",
+                    PasswordHash = passwordHash,
+                    Role = "MainAdmin"
+                }
+            );
+        }
     }
 }
---
+++ MetroLayag/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MetroLayag.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Station { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Role { get; set; } // "MainAdmin" or "StationAdmin"
+
+        [NotMapped]
+        [Required]
+        public string Password { get; set; }
+    }
+}
