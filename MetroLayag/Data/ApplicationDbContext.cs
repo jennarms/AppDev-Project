7ac53870d7e159cbb44cf2be322c59a45e7b6fb0
Jenna <148486734+jennarms@users.noreply.github.com>
2025-06-17 17:17:00 +0800

    accounts for demo

--- MetroLayag/Data/ApplicationDbContext.cs
+++ MetroLayag/Data/ApplicationDbContext.cs
@@ -17,23 +17,22 @@ namespace MetroLayag.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            var passwordHash = "$2a$11$fJ8yUYDZQsMZVc/j8JNV.e7ijmTc0S3IAYtbnmvfRMyEvN6VvFHBu"; // hashed "pass123"
-
+            //accounts for testing only
             modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Station = "Main Office", Username = "admin", PasswordHash = passwordHash, Role = "MainAdmin" },
-                new User { Id = 2, Station = "Escolta", Username = "escolta", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 3, Station = "Lawton", Username = "lawton", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 4, Station = "Quinta", Username = "quinta", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 5, Station = "PUP", Username = "pup", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 6, Station = "Sta. Ana", Username = "staana", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 7, Station = "Lambingan", Username = "lambingan", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 8, Station = "Valenzuela", Username = "valenzuela", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 9, Station = "Hulo", Username = "hulo", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 10, Station = "Guadalupe", Username = "guadalupe", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 11, Station = "Maybunga", Username = "maybunga", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 12, Station = "San Joaquin", Username = "sanjoaquin", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 13, Station = "Kalawaan", Username = "kalawaan", PasswordHash = passwordHash, Role = "StationAdmin" },
-                new User { Id = 14, Station = "Pinagbuhatan", Username = "pinagbuhatan", PasswordHash = passwordHash, Role = "StationAdmin" }
+                new User { Id = 1, Station = "Main Office", Username = "admin", PasswordHash = "admin123", Role = "MainAdmin" },
+                new User { Id = 2, Station = "Escolta", Username = "escolta", PasswordHash = "escolta123", Role = "StationAdmin" },
+                new User { Id = 3, Station = "Lawton", Username = "lawton", PasswordHash = "lawton123", Role = "StationAdmin" },
+                new User { Id = 4, Station = "Quinta", Username = "quinta", PasswordHash = "quinta123", Role = "StationAdmin" },
+                new User { Id = 5, Station = "PUP", Username = "pup", PasswordHash = "pup123", Role = "StationAdmin" },
+                new User { Id = 6, Station = "Sta. Ana", Username = "staana", PasswordHash = "staana123", Role = "StationAdmin" },
+                new User { Id = 7, Station = "Lambingan", Username = "lambingan", PasswordHash = "lambingan123", Role = "StationAdmin" },
+                new User { Id = 8, Station = "Valenzuela", Username = "valenzuela", PasswordHash = "valenzuela123", Role = "StationAdmin" },
+                new User { Id = 9, Station = "Hulo", Username = "hulo", PasswordHash = "hulo123", Role = "StationAdmin" },
+                new User { Id = 10, Station = "Guadalupe", Username = "guadalupe", PasswordHash = "guadalupe123", Role = "StationAdmin" },
+                new User { Id = 11, Station = "Maybunga", Username = "maybunga", PasswordHash = "maybunga123", Role = "StationAdmin" },
+                new User { Id = 12, Station = "San Joaquin", Username = "sanjoaquin", PasswordHash = "sanjoaquin123", Role = "StationAdmin" },
+                new User { Id = 13, Station = "Kalawaan", Username = "kalawaan", PasswordHash = "kalawaan123", Role = "StationAdmin" },
+                new User { Id = 14, Station = "Pinagbuhatan", Username = "pinagbuhatan", PasswordHash = "pinagbuhatan123", Role = "StationAdmin" }
             );
         }
     }
