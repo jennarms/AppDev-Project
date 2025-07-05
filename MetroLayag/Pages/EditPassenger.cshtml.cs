072eb9f7c152c8366031180ea18b930825a66471
Jenna <148486734+jennarms@users.noreply.github.com>
2025-07-05 23:55:19 +0800

    edit on starting and destination

--- MetroLayag/Pages/EditPassenger.cshtml.cs
+++ MetroLayag/Pages/EditPassenger.cshtml.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MetroLayag.Data;
 using MetroLayag.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MetroLayag.Pages
 {
@@ -17,11 +20,29 @@ namespace MetroLayag.Pages
         [BindProperty]
         public Passenger Passenger { get; set; }
 
+        public string UserStation { get; set; } = string.Empty;
+
+        public List<string> OtherStations { get; set; } = new List<string>();
+
+        private static readonly List<string> AllStations = new List<string>
+        {
+            "Escolta", "Lawton", "Quinta", "PUP", "Sta. Ana", "Lambingan",
+            "Valenzuela", "Hulo", "Guadalupe", "Maybunga", "San Joaquin",
+            "Kalawaan", "Pinagbuhatan"
+        };
+
         public IActionResult OnGet(int id)
         {
             Passenger = _context.Passengers.Find(id);
             if (Passenger == null)
                 return NotFound();
+
+            var sessionStation = HttpContext.Session.GetString("Station");
+            if (string.IsNullOrEmpty(sessionStation))
+                return RedirectToPage("/Login");
+
+            UserStation = sessionStation;
+            OtherStations = AllStations.Where(s => s != UserStation).ToList();
 
             return Page();
         }
